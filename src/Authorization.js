52aa7d912ea1baf8bac00cd7c72bb444b0a1ff6b
Kateryna083 <katyasmile083@gmail.com>
2024-09-07 16:51:44 +0300

    authorization

--- src/Authorization.js
+++ src/Authorization.js
@@ -1,21 +1,64 @@
 import { axios } from "axios";
 
 const accessKey = "2218e48bf59c52704334d796d79c406e";
-const url =
-  "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1";
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
 
-const options = {
-  headers: {
-    // Замість api_read_access_token вставте свій токен
-    // Authorization: "Bearer api_read_access_token",
-    Authorization: `Bearer ${accessKey}`,
-  },
-};
+export async function getTrendingMovie() {
+  const response = await axios.get("/trending/movie/day", {
+    params: { language: "en-US" },
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMjE4ZTQ4YmY1OWM1MjcwNDMzNGQ3OTZkNzljNDA2ZSIsIm5iZiI6MTcyNTU0NTU2Ni45NzgwMzQsInN1YiI6IjY2ZDliYTFmZDdjMTllYTg2NmI4ZDJmNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vyEt0V0IujxaNXEqDmfd847z3XCoeAsDhgBK5wrYJIE",
+    },
+  });
+  return response.data;
+}
 
-axios
-  .get(url, options)
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+export function searchMovie(query) {
+  return axios.get("/search/movie", {
+    params: { language: "en-US", query },
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMjE4ZTQ4YmY1OWM1MjcwNDMzNGQ3OTZkNzljNDA2ZSIsIm5iZiI6MTcyNTU0NTU2Ni45NzgwMzQsInN1YiI6IjY2ZDliYTFmZDdjMTllYTg2NmI4ZDJmNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vyEt0V0IujxaNXEqDmfd847z3XCoeAsDhgBK5wrYJIE",
+    },
+  });
+}
+
+export async function getMovieDetail(movieId) {
+  const response = await axios.get(`/movie/${movieId}`, {
+    params: { language: "en-US" },
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMjE4ZTQ4YmY1OWM1MjcwNDMzNGQ3OTZkNzljNDA2ZSIsIm5iZiI6MTcyNTU0NTU2Ni45NzgwMzQsInN1YiI6IjY2ZDliYTFmZDdjMTllYTg2NmI4ZDJmNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vyEt0V0IujxaNXEqDmfd847z3XCoeAsDhgBK5wrYJIE",
+    },
+  });
+  return response.data;
+}
+
+export async function getMovieCast(movieId) {
+  return await axios.get(`/movie/${movieId}/credits`, {
+    params: { language: "en-US" },
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMjE4ZTQ4YmY1OWM1MjcwNDMzNGQ3OTZkNzljNDA2ZSIsIm5iZiI6MTcyNTU0NTU2Ni45NzgwMzQsInN1YiI6IjY2ZDliYTFmZDdjMTllYTg2NmI4ZDJmNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vyEt0V0IujxaNXEqDmfd847z3XCoeAsDhgBK5wrYJIE",
+    },
+  });
+}
+
+export async function getReviews(movieId) {
+  return await axios.get(`/movie/${movieId}/reviews`, {
+    params: { language: "en-US" },
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMjE4ZTQ4YmY1OWM1MjcwNDMzNGQ3OTZkNzljNDA2ZSIsIm5iZiI6MTcyNTU0NTU2Ni45NzgwMzQsInN1YiI6IjY2ZDliYTFmZDdjMTllYTg2NmI4ZDJmNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vyEt0V0IujxaNXEqDmfd847z3XCoeAsDhgBK5wrYJIE",
+    },
+  });
+}
 
 //   Ключ API
 // 2218e48bf59c52704334d796d79c406e
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,7 @@ import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
 import MoviesPage from "../../pages/MoviesPage/MoviesPage";
 
 import "./App.css";
+import css from "./Oooo.module.css";
 
 export default function App() {
   return (
